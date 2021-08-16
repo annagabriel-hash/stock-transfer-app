8c1b416600d7e89b24d9b174cc051c3caef61e56
Anna Gabriel <yssgabriel@gmail.com>
2021-08-16 16:15:52 +0800

    Spec user valid attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password_confirmation: 'password')}
+
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
 end
