94e5c3896c9c52cb4bce4fb4a433909ed2f73c8c
Anna Gabriel <yssgabriel@gmail.com>
2021-08-16 17:14:52 +0800

    Update user model test to include password #9

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { described_class.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password_confirmation: 'password') }
+  let(:user) { described_class.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password: 'password', password_confirmation: 'password') }
 
   it 'is valid with valid attributes' do
     expect(user).to be_valid
