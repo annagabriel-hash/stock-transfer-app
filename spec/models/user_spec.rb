2818869c08ed23498fcac3b0f95f3e59635c9522
Anna Gabriel <yssgabriel@gmail.com>
2021-08-16 16:20:26 +0800

    Fix format based on rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {User.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password_confirmation: 'password')}
+  let(:user) { described_class.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password_confirmation: 'password') }
 
   it 'is valid with valid attributes' do
     expect(user).to be_valid
   end
-
 end
