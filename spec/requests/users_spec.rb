0a3d24d89103bbd871d299256770a3b6a754787d
Anna Gabriel <yssgabriel@gmail.com>
2021-08-16 20:12:33 +0800

    Specify user controller post action #9

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersController', type: :request do
+  let(:user) { User.new(first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password: 'password', password_confirmation: 'password') }
+
   describe 'GET /new' do
     before do
       get new_user_registration_path
@@ -13,6 +15,31 @@ RSpec.describe 'UsersController', type: :request do
     it 'renders' do
       get new_user_registration_path
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST /create' do
+    before do
+      post user_registration_path(user), params: { user: { first_name: 'John', last_name: 'Doe', email: 'johndoe@example.com', password: 'password', password_confirmation: 'password' } }
+    end
+
+    it 'is works' do
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it 'creates new user with email' do
+      created_user = User.where({ email: 'johndoe@example.com' })
+      expect(created_user).to exist
+    end
+
+    it 'creates new user with first name' do
+      created_user = User.find_by(email: 'johndoe@example.com')
+      expect(created_user.first_name).to eq('John')
+    end
+
+    it 'creates new user with last name' do
+      created_user = User.find_by(email: 'johndoe@example.com')
+      expect(created_user.last_name).to eq('Doe')
     end
   end
 end
