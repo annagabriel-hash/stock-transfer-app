c5841a92fb48818d878ceda135ecae3cacf65fc9
Anna Gabriel <yssgabriel@gmail.com>
2021-08-17 19:20:18 +0800

    Specify showing of user edit page #3

--- spec/system/list_users_spec.rb
+++ spec/system/list_users_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "ListUsers", type: :system do
-  before do
-    driven_by(:rack_test)
-    user_list = build_list(:user, 10) do |user, i|
+RSpec.describe 'ListUsers', type: :system do
+  let(:user_list) do
+    build_list(:user, 10) do |user, i|
       user.email = "person#{i}@example.com"
       user.save
     end
+  end
+
+  before do
+    driven_by(:rack_test)
     sign_in user_list.first
     visit users_path
   end
@@ -15,5 +18,10 @@ RSpec.describe "ListUsers", type: :system do
     within 'tbody' do
       expect(page).to have_selector('tr', count: 10)
     end
+  end
+
+  it 'shows edit user page when edit button is clicked' do
+    find_link('Edit', href: edit_user_registration_path(user_list.first)).click
+    expect(page).to have_current_path(edit_user_registration_path(user_list.first))
   end
 end
