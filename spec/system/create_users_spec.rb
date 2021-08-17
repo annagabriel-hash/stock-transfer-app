e30a8ac74aebdf87a234b9b623f4dbd934c766e3
Anna Gabriel <yssgabriel@gmail.com>
2021-08-17 10:56:28 +0800

    Specify required first name for user #10

--- spec/system/create_users_spec.rb
+++ spec/system/create_users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'CreateUsers', type: :system do
   before do
-    driven_by :rack_test
+    driven_by :selenium, using: :chrome
     # Fill in form
     fill_in_signup_form(build(:user))
   end
@@ -13,6 +13,14 @@ RSpec.describe 'CreateUsers', type: :system do
       expect(page).to have_current_path(root_path)
       expect(page).to have_content('Welcome! You have signed up successfully')
       expect(page).to have_content('John Doe')
+    end
+  end
+
+  context 'without first name', :aggregate_failures do
+    it 'generates an error message and render sign in page' do
+      fill_in 'First name', with: ' '
+      expect { click_on 'Sign up' }.not_to change(User, :count)
+      expect(page).to have_content('First name can\'t be blank')
     end
   end
 end
