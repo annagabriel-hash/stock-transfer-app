88f31a6e34a148bed21ad1f479bcaf4548197e1d
Anna Gabriel <yssgabriel@gmail.com>
2021-08-17 08:35:14 +0800

    Add full name method to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   it 'is valid with valid attributes' do
     expect(build(:user)).to be_valid
+  end
+
+  it 'has full name attribute' do
+    expect(create(:user).full_name).to eq('John Doe')
   end
 end
