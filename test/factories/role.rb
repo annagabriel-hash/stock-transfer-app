f0421bea72bef97787bb6bbb259b29f299a833cc
Anna Gabriel <yssgabriel@gmail.com>
2021-08-19 10:20:38 +0800

    Update role test data to create role if not existing

--- test/factories/role.rb
+++ test/factories/role.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :role do
     name { 'buyer' }
+    initialize_with { Role.find_or_create_by(name: name) }
+  end
 
-    trait :admin do
-      name { 'admin' }
-    end
+  trait :admin do
+    name { 'admin' }
+  end
 
-    trait :broker do
-      name { 'broker' }
-    end
+  trait :broker do
+    name { 'broker' }
   end
 end
