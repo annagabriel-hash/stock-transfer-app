22f0c40bbef86e086c84fb4d8ce488cdb3dd7cad
Anna Gabriel <yssgabriel@gmail.com>
2021-08-19 12:08:33 +0800

    Update list user spec Create default role Sign in as admin Update links

--- spec/system/list_users_spec.rb
+++ spec/system/list_users_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'ListUsers', type: :system do
-  let(:user_list) do
+  let!(:user_list) do
+    create(:role)
     build_list(:user, 10) do |user, i|
       user.email = "person#{i}@example.com"
       user.save!
@@ -10,14 +11,14 @@ RSpec.describe 'ListUsers', type: :system do
 
   before do
     driven_by(:rack_test)
-    create(:role)
-    sign_in user_list.first
-    visit users_path
+    admin_user = create(:user, :admin)
+    sign_in admin_user
+    visit admin_users_path
   end
 
   it 'displays all registered users' do
     within 'tbody' do
-      expect(page).to have_selector('tr', count: 10)
+      expect(page).to have_selector('tr', count: 11)
     end
   end
 
@@ -27,7 +28,7 @@ RSpec.describe 'ListUsers', type: :system do
   end
 
   it 'view user information when view button is clicked' do
-    find_link('View', href: user_path(user_list.second)).click
-    expect(page).to have_current_path(user_path(user_list.second))
+    find_link('View', href: admin_user_path(user_list.second)).click
+    expect(page).to have_current_path(admin_user_path(user_list.second))
   end
 end
