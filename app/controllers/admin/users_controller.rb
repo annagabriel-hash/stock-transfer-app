288123ba01c82fa14ed99b37a846ce3b7623baef
Anna Gabriel <yssgabriel@gmail.com>
2021-08-19 16:06:52 +0800

    Update admin controller to admin role #27

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,16 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :require_admin
 
   def index
     @users = User.all
   end
 
   def show; end
+
+  def require_admin
+    return if current_user.admin?
+
+    flash[:alert] = 'You are not authorized to perform that action'
+    redirect_to root_path
+  end
 end
