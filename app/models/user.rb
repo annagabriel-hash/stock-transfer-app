d32d701fe047531029c5663ef51a49874423cdfa
Anna Gabriel <yssgabriel@gmail.com>
2021-08-19 10:21:50 +0800

    Update user model to set default role before creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,15 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   has_many :user_roles, dependent: :destroy
   has_many :roles, through: :user_roles
+  before_create :set_default_role
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def set_default_role
+    roles << Role.find_by(name: 'buyer') if roles.empty?
   end
 end
