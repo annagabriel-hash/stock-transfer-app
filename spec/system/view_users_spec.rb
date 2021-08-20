642853636242447bee31fe0e5a0a82f25669ec8a
Anna Gabriel <yssgabriel@gmail.com>
2021-08-20 08:02:48 +0800

    Specify buyer/broker restriction when accessing other user profile

--- spec/system/view_users_spec.rb
+++ spec/system/view_users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "ViewUsers", type: :system do
+RSpec.describe 'ViewUsers', type: :system do
   let(:admin_user) { create(:user, :admin) }
   let!(:buyer) { create(:user) }
 
@@ -10,14 +10,25 @@ RSpec.describe "ViewUsers", type: :system do
     visit admin_user_path(buyer)
   end
 
-  it 'renders the user profile page when view button is clicked' do
-    expect(page).to have_current_path(admin_user_path(buyer))
+  context 'when user is an admin' do
+    it 'renders the user profile page when view button is clicked' do
+      expect(page).to have_current_path(admin_user_path(buyer))
+    end
+
+    it 'view user information when view button is clicked', aggregate_failures: true do
+      expect(page).to have_content(buyer.email)
+      expect(page).to have_content(buyer.first_name)
+      expect(page).to have_content(buyer.last_name)
+      expect(page).to have_content(buyer.roles.first.name)
+    end
   end
 
-  it 'view user information when view button is clicked', aggregate_failures: true do
-    expect(page).to have_content(buyer.email)
-    expect(page).to have_content(buyer.first_name)
-    expect(page).to have_content(buyer.last_name)
-    expect(page).to have_content(buyer.roles.first.name)
+  context 'when user is not an admin' do
+    it 'does not render the user profile page and redirects to root page' do
+      sign_out admin_user
+      sign_in buyer
+      visit admin_user_path(admin_user)
+      expect(page).to have_current_path(root_path)
+    end
   end
 end
