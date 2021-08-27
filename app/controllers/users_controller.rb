9e8ca58377a7a777050d3930431d2fcef2ba2e33
Anna Gabriel <yssgabriel@gmail.com>
2021-08-27 11:00:17 +0800

    Add verify action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard; end
+
+  def verify
+    UserMailer.verify_email(current_user).deliver_later
+    flash[:notice] = 'Verification email sent'
+    redirect_to root_path
+  end
 end
