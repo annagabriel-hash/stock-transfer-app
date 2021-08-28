dcac608bc16415ba59902cce794bc5bfb2aeb2b4
Anna Gabriel <yssgabriel@gmail.com>
2021-08-28 08:00:25 +0800

    Update action mailer config for prod and dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,4 +122,6 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
