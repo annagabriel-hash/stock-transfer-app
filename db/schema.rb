5a9624a96417149ed7baa6da2c6ca133bff0f8d7
Anna Gabriel <yssgabriel@gmail.com>
2021-08-31 13:31:18 +0800

    Add stock model #53

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_021016) do
+ActiveRecord::Schema.define(version: 2021_08_31_051104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2021_08_26_021016) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker", null: false
+    t.string "name"
+    t.float "price"
+    t.float "closing_price"
+    t.float "change"
+    t.float "change_percent"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "user_roles", force: :cascade do |t|
