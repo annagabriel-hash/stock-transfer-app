af44805f2bfdca5e0a073855267c23be30debf11
Anna Gabriel <yssgabriel@gmail.com>
2021-09-02 19:08:28 +0800

    Update credentials

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,6 +50,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.filter_sensitive_data('test-iex-api-publishable-token') { Rails.application.credentials.publishable_token }
-  c.filter_sensitive_data('test-iex-api-secret-token') { Rails.application.credentials.secret_token }
+  c.filter_sensitive_data('test-iex-api-publishable-token') { ENV.fetch('IEX_PUBLISHABLE_TOKEN') { Rails.application.credentials.publishable_token } }
+  c.filter_sensitive_data('test-iex-api-secret-token') { ENV.fetch('IEX_SECRET_TOKEN') { Rails.application.credentials.secret_token } }
 end
