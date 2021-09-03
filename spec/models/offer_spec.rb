f60f0f243333033bc180b2b231f8d4038031814b
Anna Gabriel <yssgabriel@gmail.com>
2021-09-03 13:04:54 +0800

    Specify offer model validations #60

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Offer, type: :model do
   it { is_expected.to be_open }
   it { is_expected.to be_market_order }
   it { is_expected.to be_buy }
+
+  context 'with invalid attributes' do
+    it 'is not valid without order type' do
+      offer.type = nil
+      expect(offer).not_to be_valid
+    end
+
+    it 'is not valid without action' do
+      offer.action = nil
+      expect(offer).not_to be_valid
+    end
+  end
 end
