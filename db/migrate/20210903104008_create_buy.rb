0d0603d8ecd9b34450755cb2ef45c464dafe4d63
Anna Gabriel <yssgabriel@gmail.com>
2021-09-06 17:53:57 +0800

    Add buy model

---
+++ db/migrate/20210903104008_create_buy.rb
@@ -0,0 +1,14 @@
+class CreateBuy < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buys do |t|
+      t.references :stock, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.float :price, null: false
+      t.integer :shares, null: false
+      t.integer :status, null: false, default: 0
+      t.integer :order_type, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
