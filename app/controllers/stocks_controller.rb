7b9c12954e201cd3b54ccd61b5d3a550ff4af978
Anna Gabriel <yssgabriel@gmail.com>
2021-09-07 13:36:08 +0800

    Update stock instantiate sell and buy models

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,8 @@ class StocksController < ApplicationController
   def search
     if params[:ticker]
       @stock = Stock.lookup(params[:ticker])
+      @sell = Sell.new
+      @buy = Buy.new
       unless @stock
         flash[:alert] = 'Please enter a valid symbol to search'
         redirect_to root_path
