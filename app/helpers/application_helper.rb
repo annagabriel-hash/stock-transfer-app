333c2ba1145880b8053a174da62a2f113b08103b
Anna Gabriel <yssgabriel@gmail.com>
2021-09-07 15:05:19 +0800

    Given that I'm a broker, when I go to the stocks page, I should not be able to enter shares more than what I own. Fixes #63

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def max_shares(price)
     (current_user.balance / price).floor
   end
+
+  def shares_owned(ticker)
+    stock = Stock.where(ticker: ticker).first
+    user_stock = UserStock.where(user: current_user, stock: stock).first
+    user_stock.shares
+  rescue StandardError
+    0
+  end
 end
