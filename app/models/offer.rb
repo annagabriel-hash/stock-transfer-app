6d18a54e401297bd8da286ba39ef23cef72dc214
Anna Gabriel <yssgabriel@gmail.com>
2021-09-07 13:30:14 +0800

    Remove offer model

--- app/models/offer.rb
+++
@@ -1,9 +0,0 @@
-class Offer < ApplicationRecord
-  belongs_to :stock
-  belongs_to :user
-  enum status: { open: 0, close: 1, cancelled: 2 }
-  enum type: { limit_order: 0, market_order: 1 }
-  enum action: { buy: 0, sell: 1 }
-  validates :type, presence: true
-  validates :action, presence: true
-end
