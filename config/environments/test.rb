5512620adae2e2d0351327aede78f5796d7c640f
Anna Gabriel <yssgabriel@gmail.com>
2021-10-06 15:53:06 +0800

    Update test config api details

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,6 +51,6 @@ Rails.application.configure do
   IEX::Api.configure do |config|
     config.publishable_token = ENV['IEX_PUBLISHABLE_TOKEN']
     config.secret_token = ENV['IEX_SECRET_TOKEN']
-    config.endpoint = 'https://sandbox.iexapis.com/v1'
+    config.endpoint = 'https://cloud.iexapis.com/v1'
   end
 end
